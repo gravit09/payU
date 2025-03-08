c7ddfe09fab368b6681cfc4e3fd0606963743a76
Gravit09 <garvitchoudhary09@gmail.com>
2025-03-08 18:05:26 +0530

    fix2

--- apps/client/app/api/auth/[...nextauth]/route.ts
+++ apps/client/app/api/auth/[...nextauth]/route.ts
@@ -72,6 +72,6 @@ export const authOptions: NextAuthOptions = {
   },
 };
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
